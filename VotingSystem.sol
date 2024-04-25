4e79dedd57f3efffde0c8cb94e069f90174d20e5
shriy14 <126308014+shriy14@users.noreply.github.com>
2024-04-25 22:35:05 +0530

    Update VotingSystem.sol

--- VotingSystem.sol
+++ VotingSystem.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+//helllllllllllllllllloooooooooooooooooooooooooooooooooooooooooooooo
 contract VotingSystem {
     // Struct to represent a voter
     struct Voter {
